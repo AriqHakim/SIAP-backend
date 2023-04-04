b5d9f461592383bf5629289df03113d1e9203e06
AriqHakim <ariq.hakim2002@gmail.com>
2023-04-04 11:53:45 +0700

    add typeorm config

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import AppDataSource from "./orm.config";
+import ExpressServer, { Express, Router } from "express";
+import cors from "cors";
+
+const PORT: number = parseInt(process.env.PORT || "3000");
+
+async function main() {
+  // await initializeDatabase(ormConfig)
+  // const init_router: Router = await initializeEndpoints()
+  const server: Express = ExpressServer();
+  await AppDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err);
+    });
+  server.use(cors());
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  //   server.use("/");
+}
+
+main();
--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,8 +1,18 @@
 import * as dotenv from "dotenv";
+dotenv.config();
 import { DataSource } from "typeorm";
 
 dotenv.config();
 
-export const ormConfig: DataSource = new DataSource();
+export const AppDataSource: DataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: "siap",
+  entities: [__dirname + "/entity/**/*{.js,.ts}"],
+  migrations: [__dirname + "/migration/**/*{.js,.ts}"],
+});
 
-export default ormConfig;
+export default AppDataSource;
